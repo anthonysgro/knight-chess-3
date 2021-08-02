ac453f9e5f6725d07c437c05d29255728e03601d
Anthony <AnthonySgro1995@gmail.com>
2021-08-02 00:58:06 -0400

    room states

--- server/game/createInitGameState.js
+++ server/game/createInitGameState.js
@@ -1,6 +1,7 @@
 const parseState = require("./parseState");
 function createInitGameState(gameCode, initState, roomStates) {
     roomStates[gameCode] = parseState(initState);
+    console.log(roomStates[gameCode]);
 }
 
 module.exports = createInitGameState;
