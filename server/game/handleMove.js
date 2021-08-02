d15db96ff91ff973e6c6cc42ffd12bd56b2979e9
Anthony <AnthonySgro1995@gmail.com>
2021-08-02 14:32:48 -0400

    debugging on move handling

--- server/game/handleMove.js
+++ server/game/handleMove.js
@@ -2,8 +2,14 @@ const parseState = require("./parseState");
 
 function handleMove(newState, gameCode, playerId, roomStates, server) {
     roomStates[gameCode] = newState;
-    console.log("****");
+    console.log("*******");
     console.log("PLAYER MOVED");
+    if (roomStates[gameCode]) {
+        console.log(JSON.stringify(newState).substring(0, 50));
+    } else {
+        console.log("No state sent on move somehow...");
+    }
+    console.log("*******");
     server.to(gameCode).emit("playerMoved", newState, playerId); // broadcast new state
 }
 
