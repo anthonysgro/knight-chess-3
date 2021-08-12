fc9cb290a47aa84f7854cf0c9f25bce36cda250c
Anthony <AnthonySgro1995@gmail.com>
2021-08-12 10:53:28 -0400

    figuring out the bug

--- server/game/createInitGameState.js
+++ server/game/createInitGameState.js
@@ -3,7 +3,8 @@ function createInitGameState(gameCode, initState, roomStates) {
     if (roomStates[gameCode]) {
         console.log(
             "CREATE INIT GAME STATE",
-            roomStates[gameCode].substring(0, 100),
+            gameCode,
+            "\n" + roomStates[gameCode].substring(0, 100),
         );
     } else {
         console.log(" CREATE INIT GAME STATE:", "IT ISNT HERE!");
